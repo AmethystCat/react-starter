8bc4e1d7fd6d31e9ca82961de333a1d1fe27ccd6
HC <lovelifeloveyoumore@gmail.com>
2016-12-23 23:46:21 +0800

    add test helper

---
+++ test/helper.js
@@ -0,0 +1,16 @@
+/**
+ * test setup
+ */
+import {jsdom} from 'jsdom';
+let exposedProperties = ['window', 'navigator', 'document'];
+
+global.document = jsdom();
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+    if (typeof global[property] === 'undefined') {
+        global[property] = document.defaultView[property];
+    }
+});
+global.navigator = {
+    userAgent: 'node.js'
+};
