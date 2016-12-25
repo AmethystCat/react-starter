a03a3bc22294ad05c84275448b3a5ef93e529ba0
HC <lovelifeloveyoumore@gmail.com>
2016-12-25 23:26:31 +0800

    test fixed

--- test/test.test.js
+++ test/test.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import {chai} from 'chai';
+import {expect} from 'chai';
 import Test from '../src/test.jsx';
  describe('testting', function() {
  	it('test jsx 测试', function(){
- 		const T = shallow(Test);
+ 		const T = shallow(<Test/>);
  		expect(T.text()).to.equal('hello react');
  	});
  });
