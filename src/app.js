8135dec52c3117bae13a7a5013dc3beb28c946ff
HC <huangchun_cd@keruyun.com>
2018-07-04 20:45:03 +0800

    updated to parcel starter config

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const HelloWorld = () => <h1>Hello HC 😋 🍱 🍛 🍲</h1>;
+
+ReactDOM.render(<HelloWorld />, document.getElementById('app'));
